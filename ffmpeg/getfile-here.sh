461348f9724dd6b95457c2d7c9f8537cad247e37
Tomer Baum <tbaumm@proofpoint.com>
2025-10-24 13:15:50 +0300

    update the files code update ffmpeg/getfile-here.sh file plus update 1 files

--- ffmpeg/getfile-here.sh
+++ ffmpeg/getfile-here.sh
@@ -3,6 +3,7 @@ folder="1#1-mgaraqb7_nn5d7va_32"
 rm -rf /tmp/a/
 mkdir /tmp/a
 scp baum@192.168.0.155:/home/node/tts/background/back-45.mp4 /tmp/a/
+scp baum@192.168.0.155:/home/node/tts/background/key-long1.mp3 /tmp/a/
 
 scp baum@192.168.0.155:/home/node/tts/$folder/before.mp3 /tmp/a/
 scp baum@192.168.0.155:/home/node/tts/$folder/after.mp3 /tmp/a/
--- ffmpeg/runit.sh
+++ ffmpeg/runit.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 #testing before section 
-./ffmpeg-run.sh one_mp3 /tmp/a/back-45.mp4 /tmp/a/before.mp3 /mnt/c/ffmpeg/output.mp4
-
+#./ffmpeg-run.sh one_mp3 /tmp/a/back-45.mp4 /tmp/a/before.mp3 /mnt/c/ffmpeg/before.mp4
 # testing is good !
 
+
+
+# mp3 mp4 and key.mp3 
+#./ffmpeg-run.sh two_mp3 /tmp/a/back-45.mp4 /tmp/a/before.mp3 /tmp/a/key-long1.mp3 3 /mnt/c/ffmpeg/before-and-keys.mp4
+# good testing 
+
+
+./ffmpeg-run.sh concat "/tmp/output1.mp4" "/mnt/c/ffmpeg/before.mp4" "/mnt/c/ffmpeg/before-and-keys.mp4" 
