267deb14bf0730d69429008037c912d202665380
Tomer Baum <tbaumm@proofpoint.com>
2025-10-24 23:19:21 +0300

    good-master7

--- ffmpeg/runit.sh
+++ ffmpeg/runit.sh
@@ -1,54 +1,32 @@
 #!/bin/bash
 
+output_folder=${output_folder:-/mnt/c/ffmpeg}
+backup_folder=${backup_folder:-/tmp/a}
+font_folder=${font_folder:-/tmp/a/fonts}
+
+
 good(){
+  # mp3 mp4 and key.mp3 
+  ./ffmpeg-run.sh two_mp3 ${backup_folder}/back-45.mp4 ${output_folder}/before.mp3 ${output_folder}/key-long1.mp3 3 ${output_folder}/before-and-keys.mp4
+  # good testing 
 
-
-echo .
-
-# mp3 mp4 and key.mp3 
-./ffmpeg-run.sh two_mp3 /tmp/a/back-45.mp4 /tmp/a/before.mp3 /tmp/a/key-long1.mp3 3 /mnt/c/ffmpeg/before-and-keys.mp4
-# good testing 
-
-
-
-
+}
+code_run_verb(){
+./ffmpeg-run.sh running_code \
+  ${output_folder}/frozen-7s.mp4 \
+  ${output_folder}/running_script.txt \
+  50 1200 \
+  3 \
+  1.2 \
+  ${output_folder}/running-code-demo.mp4 \
+  '' \
+ ${output_folder}/coderun.mp3 
 }
 
 
 #testing before section 
-./ffmpeg-run.sh one_mp3 /tmp/a/back-45.mp4 /tmp/a/before.mp3 /mnt/c/ffmpeg/before.mp4
+./ffmpeg-run.sh one_mp3 ${backup_folder}/back-45.mp4 ${output_folder}/before.mp3 ${output_folder}/before.mp4
 # testing is good !
-
-
-#good
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -56,45 +34,38 @@ echo .
 filter1() { 
  number_lines=5   
  ./ffmpeg-run.sh filter_script \
-  /tmp/a/back-45.mp4 \
-  /tmp/a/files/filters.txt \
-  /tmp/a/code.mp3 \
-  /tmp/a/key-2s.wav \
+  ${backup_folder}/back-45.mp4 \
+  ${output_folder}/files/filters.txt \
+  ${output_folder}/code.mp3 \
+  ${backup_folder}/key-2s.wav \
   $number_lines \
-  /mnt/c/ffmpeg/output-code.mp4
+  ${output_folder}/output-code.mp4
 
 }
 
 
 
 
-
-
-
-
-code_run_verb(){
-./ffmpeg-run.sh running_code \
-  /mnt/c/ffmpeg/frozen-7s.mp4 \
-  /tmp/a/running_script.txt \
-  50 1200 \
-  3 \
-  1.2 \
-  /mnt/c/ffmpeg/running-code-demo.mp4 \
-  '' \
- /tmp/a/coderun.mp3 
-}
-
-export RC_FONTFILE="/tmp/a/fonts/BebasNeue-Regular.ttf"
+export RC_FONTFILE="${font_folder}/BebasNeue-Regular.ttf"
 export RC_FONTSIZE=66
 #https://www.colorhexa.com/27d3f5
 export RC_FONTCOLOR="#27d3f5"
 
 #code 
+
+
+# 1) Persist text from its start time (remove end-boundary gaps)
+sed -E -i "s/enable='between\(t,([0-9.]+),([0-9.]+)\)'/enable='gte(t,\1)'/g" ${output_folder}/files/filters.txt
+
+# 2) Make sure drawtext reloads the text files (prevents stale/bad frames)
+sed -E -i "s/:enable='/:reload=1:enable='/g" ${output_folder}/files/filters.txt
+
 filter1
 
-./ffmpeg-run.sh freeze_last_frame /mnt/c/ffmpeg/output-code.mp4 60 /mnt/c/ffmpeg/frozen-code-60s.mp4
 
-export RC_FONTFILE="/tmp/a/fonts/BebasNeue-Regular.ttf"
+./ffmpeg-run.sh freeze_last_frame ${output_folder}/output-code.mp4 60 ${output_folder}/frozen-code-60s.mp4
+
+export RC_FONTFILE="${font_folder}/BebasNeue-Regular.ttf"
 export RC_FONTSIZE=48
 #https://www.colorhexa.com/27d3f5
 export RC_FONTCOLOR="#ffcc00@0.95"
@@ -111,27 +82,27 @@ export RC_FONTCOLOR="#ffcc00@0.95"
 
 code_run_vera(){
 ./ffmpeg-run.sh running_code \
-  /mnt/c/ffmpeg/frozen-code-60s.mp4 \
-  /tmp/a/running_script.txt \
+  ${output_folder}/frozen-code-60s.mp4 \
+  ${output_folder}/running_script.txt \
   50 1200 \
   3 \
   1.2 \
-  /mnt/c/ffmpeg/running-code-demo.mp4 \
-  /tmp/a/gong.mp3 \
- /tmp/a/coderun.mp3 
+  ${output_folder}/running-code-demo.mp4 \
+  ${backup_folder}/gong.mp3 \
+ ${output_folder}/coderun.mp3 
 }
 
 # code run 
 code_run_vera
 
 
-./ffmpeg-run.sh freeze_last_frame /mnt/c/ffmpeg/running-code-demo.mp4 60 /mnt/c/ffmpeg/frozen-run-60s.mp4
+./ffmpeg-run.sh freeze_last_frame ${output_folder}/running-code-demo.mp4 60 ${output_folder}/frozen-run-60s.mp4
 
 
 #after 
-./ffmpeg-run.sh one_mp3 /mnt/c/ffmpeg/frozen-run-60s.mp4 /tmp/a/after.mp3 /mnt/c/ffmpeg/after.mp4
+./ffmpeg-run.sh one_mp3 ${output_folder}/frozen-run-60s.mp4 ${output_folder}/after.mp3 ${output_folder}/after.mp4
 
 
 #check combine 
-./ffmpeg-run.sh concat "/mnt/c/ffmpeg/master-output-combine.mp4" "/mnt/c/ffmpeg/before.mp4" "/mnt/c/ffmpeg/output-code.mp4" /mnt/c/ffmpeg/running-code-demo.mp4 /mnt/c/ffmpeg/after.mp4
+./ffmpeg-run.sh concat "${output_folder}/master-output-combine.mp4" "${output_folder}/before.mp4" "${output_folder}/output-code.mp4" ${output_folder}/running-code-demo.mp4 ${output_folder}/after.mp4
 #good  testing
