95b23cb579d99719fb75901de9dfd64b59baa3d3
Tomer Baum <tbaumm@proofpoint.com>
2025-11-28 21:38:49 +0200

    Modifying the Git source code update project3/ffmpeg-script.sh file

--- project3/ffmpeg-script.sh
+++ project3/ffmpeg-script.sh
@@ -20,9 +20,10 @@ func1(){
     local pic1="/mnt/c/share/pic1.png"
     local pic2="/mnt/c/share/pic2.png"
     local output_mp4="/mnt/c/share/out.mp4"
-    local text="tomer baum kuku Sample Text tomer baum aviv ariel hana lola sarah tomer baum hana haim doda lala mamani nonori"
-    local font_file="/mnt/c/share/font.ttf"
+    local text="We thought this motel was empty… until an old woman appeared from nowhere—and seconds later, my nose exploded with blood."
+    local font_file="/mnt/c/share/font1.otf"
     local font_color="white"
+    local font_size=48
     
 
     # wrap text into up to 4 lines, about 4-5 words per line
@@ -33,24 +34,22 @@ func1(){
 
     local wrapped_text=""
     local count=0
-    local line=1
-    local max_lines=4
     local max_per_line=5
 
 
      for w in "${words[@]}"; do
         if [ -z "$wrapped_text" ]; then
-        wrapped_text="$w"
-        count=1
-        else
-        if [ $count -ge $max_per_line ] && [ $line -lt $max_lines ]; then
-            wrapped_text="${wrapped_text}\\n$w"
+            wrapped_text="$w"
             count=1
-            ((line++))
         else
-            wrapped_text+=" $w"
-            ((count++))
-        fi
+            if [ $count -ge $max_per_line ]; then
+                # start a new line with a real newline character
+                wrapped_text="${wrapped_text}"$'\n'"$w"
+                count=1
+            else
+                wrapped_text+=" $w"
+                ((count++))
+            fi
         fi
     done
     # after building wrapped_text
@@ -65,9 +64,10 @@ func1(){
                         [1:v]trim=0:1.5,setpts=PTS-STARTPTS[p1b]; \
                         [p1a][p2][p1b]concat=n=3:v=1:a=0[pseq]; \
                         [0:v]trim=0:5,setpts=PTS-STARTPTS[basev]; \
+                        [basev]drawbox=x=0:y=0:w=iw:h=ih/2:color=black@1:t=fill[baseb]; \
                         [pseq]scale=iw:iw*9/16[p16]; \
-                        [basev][p16]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2[ovl]; \
-                        [ovl]drawtext=fontfile=$font_file:text='$wrapped_text_escaped':fontcolor=$font_color:fontsize=48:x=(w-text_w)/2:y=50[vout]; \
+                        [baseb][p16]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2[ovl]; \
+                        [ovl]drawtext=fontfile=$font_file:text='$wrapped_text_escaped':fontcolor=$font_color:fontsize=$font_size:x=(w-text_w)/2:y=50[vout]; \
                         [0:a]atrim=0:5,asetpts=PTS-STARTPTS[aout]" \
         -map "[vout]" -map "[aout]" \
         -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest "$output_mp4"
