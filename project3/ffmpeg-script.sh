fab1944ad2b3cf01384d3ec25e0104d21fa12a13
Tomer Baum <tbaumm@proofpoint.com>
2025-11-29 07:04:51 +0200

    Modifying the textual content of the code update project3/ffmpeg-script.sh file

--- project3/ffmpeg-script.sh
+++ project3/ffmpeg-script.sh
@@ -21,44 +21,67 @@ pic_transform(){
 echo b
 
 func1(){
-    local input_mp4="/mnt/c/share/reel1.mp4"
+    local input_mp4="/mnt/c/share/reel2.mp4"
     local pic1="/home/baum/Downloads/$pica"
     local pic2="/home/baum/Downloads/$picb"
-    local output_mp4="/mnt/c/share/out.mp4"
+    local output_mp4="/mnt/c/share/out2.mp4"
     
     local font_file="/mnt/c/share/font1.otf"
     local font_color="white"
     local font_size=48
     
 
-    # wrap text into up to 4 lines, about 4-5 words per line
+    # wrap text into up to 4 lines, about 4-5 words per line, building each line separately
     local words=()
     for w in $text; do
         words+=("$w")
     done
 
-    local wrapped_text=""
-    local count=0
     local max_per_line=5
+    local line1=""
+    local line2=""
+    local line3=""
+    local line4=""
 
+    local idx=0
+    local total=${#words[@]}
+    while [ $idx -lt $total ]; do
+        local line_index=$(( idx / max_per_line ))
+        local word=${words[$idx]}
 
-     for w in "${words[@]}"; do
-        if [ -z "$wrapped_text" ]; then
-            wrapped_text="$w"
-            count=1
-        else
-            if [ $count -ge $max_per_line ]; then
-                # start a new line with a real newline character
-                wrapped_text="${wrapped_text}"$'\n'"$w"
-                count=1
-            else
-                wrapped_text+=" $w"
-                ((count++))
-            fi
-        fi
+        case $line_index in
+            0)
+                if [ -z "$line1" ]; then
+                    line1="$word"
+                else
+                    line1+=" $word"
+                fi
+                ;;
+            1)
+                if [ -z "$line2" ]; then
+                    line2="$word"
+                else
+                    line2+=" $word"
+                fi
+                ;;
+            2)
+                if [ -z "$line3" ]; then
+                    line3="$word"
+                else
+                    line3+=" $word"
+                fi
+                ;;
+            3)
+                if [ -z "$line4" ]; then
+                    line4="$word"
+                else
+                    line4+=" $word"
+                fi
+                ;;
+        esac
+
+        idx=$(( idx + 1 ))
     done
-    # after building wrapped_text
-    wrapped_text_escaped=${wrapped_text//\'/\\\'}
 
     ffmpeg -y \
         -i "$input_mp4" \
@@ -72,7 +95,10 @@ func1(){
                         [basev]drawbox=x=0:y=0:w=iw:h=ih/2:color=black@1:t=fill[baseb]; \
                         [pseq]scale=iw:iw*9/16[p16]; \
                         [baseb][p16]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2[ovl]; \
-                        [ovl]drawtext=fontfile=$font_file:text='$wrapped_text_escaped':fontcolor=$font_color:fontsize=$font_size:x=(w-text_w)/2+60:y=50[vout]; \
+                        [ovl]drawtext=fontfile=$font_file:text=${line1@Q}:fontcolor=$font_color:fontsize=$font_size:x=(w-text_w)/2:y=40[g1]; \
+                        [g1]drawtext=fontfile=$font_file:text=${line2@Q}:fontcolor=$font_color:fontsize=$font_size:x=(w-text_w)/2:y=40+$font_size+8[g2]; \
+                        [g2]drawtext=fontfile=$font_file:text=${line3@Q}:fontcolor=$font_color:fontsize=$font_size:x=(w-text_w)/2:y=40+2*($font_size+8)[g3]; \
+                        [g3]drawtext=fontfile=$font_file:text=${line4@Q}:fontcolor=$font_color:fontsize=$font_size:x=(w-text_w)/2:y=40+3*($font_size+8)[vout]; \
                         [0:a]atrim=0:5,asetpts=PTS-STARTPTS[aout]" \
         -map "[vout]" -map "[aout]" \
         -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest "$output_mp4"
