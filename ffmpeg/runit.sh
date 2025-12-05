433c1b5a18a65389a017d19423aceb350252c219
Tomer Baum <tbaumm@proofpoint.com>
2025-12-05 19:53:39 +0200

    Modifying the Git source code update ffmpeg/runit.sh file

--- ffmpeg/runit.sh
+++ ffmpeg/runit.sh
@@ -33,7 +33,7 @@ fi
  
 
 cd ${output_folder}/
-touch $ver
+#touch $ver
 
 good(){
   # mp3 mp4 and key.mp3 
@@ -283,9 +283,122 @@ cmd_create_video_right_to_run(){
 
 }
 ########################################################  end of cmd_create_video_right_to_run
+###
+cmd_reel_from_2_pic(){
+    echo start cmd_reel_from_2_pic
+      pica=$(ls -1tr  |grep png |tail -2 |head -1 ) 
+      picb=$(ls -1tr  |grep png |tail -2 |tail -1 ) 
+     export text="${hook}"
+     #export text="I was just peeing between classes when a stranger crawled under my stall and turned a normal school day into horror"
 
 
+     #need random 
+    #local input_mp4="/home/node/tts/back_comfiui/reel2.mp4"
+    local pic1="$output_folder/$pica"
+    local pic2="$output_folder/$picb"
 
+    local n=$(( RANDOM % 5 + 1 ))
+    local input_mp4="/home/node/tts/back_comfiui/reel${n}.mp4"
+
+    local output_mp4="$output_folder/reel.mp4"
+    
+    local font_file="/home/node/tts/back_comfiui/font1.otf"
+    local font_color="white"
+    local font_size=40
+
+    # wrap text into up to 4 lines, about 4-5 words per line, building each line separately
+    local words=()
+    for w in $text; do
+        words+=("$w")
+    done
+
+    local max_per_line=5
+    local line1=""
+    local line2=""
+    local line3=""
+    local line4=""
+    local line5=""
+    local line6=""
+
+    local idx=0
+    local total=${#words[@]}
+    while [ $idx -lt $total ]; do
+        local line_index=$(( idx / max_per_line ))
+        local word=${words[$idx]}
+
+        case $line_index in
+            0)
+                if [ -z "$line1" ]; then
+                    line1="$word"
+                else
+                    line1+=" $word"
+                fi
+                ;;
+            1)
+                if [ -z "$line2" ]; then
+                    line2="$word"
+                else
+                    line2+=" $word"
+                fi
+                ;;
+            2)
+                if [ -z "$line3" ]; then
+                    line3="$word"
+                else
+                    line3+=" $word"
+                fi
+                ;;
+            3)
+                if [ -z "$line4" ]; then
+                    line4="$word"
+                else
+                    line4+=" $word"
+                fi
+                ;;
+            4)
+                if [ -z "$line5" ]; then
+                    line5="$word"
+                else
+                    line5+=" $word"
+                fi
+                ;;
+            5)
+                if [ -z "$line6" ]; then
+                    line6="$word"
+                else
+                    line6+=" $word"
+                fi
+                ;;
+        esac
+
+        idx=$(( idx + 1 ))
+    done
+
+    ffmpeg -y \
+        -i "$input_mp4" \
+        -loop 1 -i "$pic1" \
+        -loop 1 -i "$pic2" \
+        -filter_complex "[1:v]trim=0:1,setpts=PTS-STARTPTS[p1a]; \
+                        [2:v]trim=0:2.5,setpts=PTS-STARTPTS[p2]; \
+                        [1:v]trim=0:1.5,setpts=PTS-STARTPTS[p1b]; \
+                        [p1a][p2][p1b]concat=n=3:v=1:a=0[pseq]; \
+                        [0:v]trim=0:5,setpts=PTS-STARTPTS[basev]; \
+                        [basev]drawbox=x=0:y=0:w=iw:h=ih/2:color=black@1:t=fill[baseb]; \
+                        [pseq]scale=w=720:h=576[p16]; \
+                        [baseb][p16]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2[ovl]; \
+                        [ovl]drawtext=fontfile=$font_file:text=${line1@Q}:fontcolor=$font_color:fontsize=$font_size:x=(w-text_w)/2:y=120[g1]; \
+                        [g1]drawtext=fontfile=$font_file:text=${line2@Q}:fontcolor=$font_color:fontsize=$font_size:x=(w-text_w)/2:y=120+$font_size+8[g2]; \
+                        [g2]drawtext=fontfile=$font_file:text=${line3@Q}:fontcolor=$font_color:fontsize=$font_size:x=(w-text_w)/2:y=120+2*($font_size+8)[g3]; \
+                        [g3]drawtext=fontfile=$font_file:text=${line4@Q}:fontcolor=$font_color:fontsize=$font_size:x=(w-text_w)/2:y=120+3*($font_size+8)[g4]; \
+                        [g4]drawtext=fontfile=$font_file:text=${line5@Q}:fontcolor=$font_color:fontsize=$font_size:x=(w-text_w)/2:y=120+4*($font_size+8)[g5]; \
+                        [g5]drawtext=fontfile=$font_file:text=${line6@Q}:fontcolor=$font_color:fontsize=$font_size:x=(w-text_w)/2:y=120+5*($font_size+8)[vout]; \
+                        [0:a]atrim=0:5,asetpts=PTS-STARTPTS[aout]" \
+        -map "[vout]" -map "[aout]" \
+        -c:v libx264 -pix_fmt yuv420p -c:a aac -shortest "$output_mp4"
+        echo end cmd_reel_from_2_pic
+}
+
+###
 
 cmd_before(){
     echo "cmd_before"
