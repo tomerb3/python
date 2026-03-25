c3e7778972beec1100e2471b8e202a9b8861b0c6
Tomer Baum <tbaumm@proofpoint.com>
2026-03-25 21:11:28 +0200

    Modifying the Git source code update .gitignore file plus update 1 files delete lpr/lpr_fetcher/1.sh file plus delete 3 files

--- lpr/lpr_fetcher/run_fetcher.sh
+++ lpr/lpr_fetcher/run_fetcher.sh
@@ -2,7 +2,7 @@
 set -e
 
 LOCK_FILE="/tmp/lpr_fetcher.lock"
-SCRIPT_DIR="/home/baum/src/lpr/lpr_fetcher"
+SCRIPT_DIR="/home/baum/src/python_readonly/lpr/lpr_fetcher"
 LOG_FILE="$SCRIPT_DIR/fetcher.log"
 
 # Run with flock to prevent concurrent executions
